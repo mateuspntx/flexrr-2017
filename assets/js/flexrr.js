71944681115a8edda010d951ceda114da1dfdf32
Mateus Pontes <teteupl3d@gmail.com>
2017-09-09 20:59:30 -0300

    Basic changes in RenderHeaderComponent

--- assets/js/flexrr.js
+++ assets/js/flexrr.js
@@ -1,33 +1,48 @@
+const $ = function(e){
+  return document.querySelector(e)
+}
+
 const Flexrr = {}
 
 Flexrr.RenderHeaderComponent = function(showId, element){
   
-  element = document.querySelector(element);
+  element = $(element)
 
-  tmdb.call(`/tv/${showId}`, {},
-  function(showData){
+  try {
+    tmdb.call(`/tv/${showId}`, {},
+    function(showData){
+      element.innerHTML = `
+    <header class="header">
+      <div class="poster">
+        <img src="${tmdb.images_uri}/w300${showData.poster_path}" alt="">
+      </div>
+      <div class="info">
+        <h1 class="title">${showData.name} <span class="release">(${moment(showData.first_air_date).format('YYYY')})</span></h1>
+        <h3>Overview</h3>
+        <p class="overview">${showData.overview}</p>
+        <h4>Creator</h4>
+        <p class="-margin-top">${showData.created_by[0].name}</p>
+        <h4>Genres</h4>
+        <p class="-margin-top">${Flexrr.GenresMap(showData.genres)}</p>
+      </div>
+    </header>
+    <section class="seasons">
+      <div class="row"></div>
+    </section>`
+    }, 
+    function(e){
+      element.innerHTML = `
+      <header class="header">
+        <h1>Something went wrong. Try refresh the page.</h1>
+      </header>`
+    })
+  }catch(e){
+    console.error(e)
     element.innerHTML = `
-  <header class="header">
-    <div class="poster">
-      <img src="https://image.tmdb.org/t/p/w300${showData.poster_path}" alt="">
-    </div>
-    <div class="info">
-      <h1 class="title">${showData.name} <span class="release">(${moment(showData.first_air_date).format('YYYY')})</span></h1>
-      <h3>Overview</h3>
-      <p class="overview">${showData.overview}</p>
-      <h4>Creator</h4>
-      <p class="-margin-top">${showData.created_by[0].name}</p>
-      <h4>Genres</h4>
-      <p class="-margin-top">${Flexrr.GenresMap(showData.genres)}</p>
-    </div>
-  </header>
-  <section class="seasons">
-    <div class="row"></div>
-  </section>`
-  }, 
-  function(e){
-    console.log("Error: "+e)
-  })
+      <header class="header">
+        <h1>Something went wrong. Try refresh the page.</h1>
+      </header>`
+  }
 
 }
 
@@ -51,7 +66,7 @@ Flexrr.SeasonsComponent = function(data){
 
 Flexrr.RenderSeasonsComponent = function(data){
 
-  const seasonBox = document.querySelector('.row')
+  const seasonBox = $('.row')
   
   try {
     seasonBox.innerHTML = Flexrr.SeasonsComponent(data)
@@ -66,7 +81,6 @@ Flexrr.Init = function() {
   const showId = '48866'
 
   Flexrr.RenderHeaderComponent(showId, '.app')
-  //Flexrr.RenderSeasonsComponent(Flexrr.data)
 
 }
 
